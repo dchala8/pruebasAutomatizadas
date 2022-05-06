7f88a365994eafa9ddafb8e1e5fb5ed2ac22fc73
JorgeE <je.suarezg1@uniandes.edu.co>
2022-05-06 16:55:13 -0500

    Implementación caso de prueba crear tag

--- PruebasKraken/features/settings.feature
+++ PruebasKraken/features/settings.feature
@@ -1,5 +1,5 @@
 Feature: Settings
 
-@user1 @web
-Scenario: Escenario 20
-  Given I navigate to page "http://localhost:2369/ghost"
+#@user1 @web
+#Scenario: Escenario 20
+#  Given I navigate to page "http://localhost:2369/ghost"
--- PruebasKraken/features/web/step_definitions/tag_steps.js
+++ PruebasKraken/features/web/step_definitions/tag_steps.js
@@ -1,1 +1,6 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
+
+Then('I enter text {kraken-string} into field with id "name"', async function (tagName) {
+    let element = await this.driver.$('//input[@name="name"]');
+    return await element.setValue(tagName);
+});
