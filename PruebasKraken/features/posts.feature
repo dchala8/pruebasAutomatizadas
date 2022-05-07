c98b96d3be89421dcd7a1f85292feca95835cab0
jgutierrez9891 <j.gutierrezl@uniandes.edu.co>
2022-05-07 13:06:59 -0500

    Creación escenarios de prueba posts - parcial

--- PruebasKraken/features/posts.feature
+++ PruebasKraken/features/posts.feature
@@ -1,32 +1,60 @@
 Feature: Escenarios que involucran la creacion de posts
 
-@user1 @web
-Scenario: Como administrador me gustaria crear post, publicarlo, y luego modificarlo
+# @user1 @web
+# Scenario: Como administrador me gustaria crear post, publicarlo, y luego modificarlo
+#   Given I navigate to page "<GHOST_URL>"
+#   And I wait for 5 seconds
+#   When I make login with "<USER>" and "<PASSWORD>"
+#   And I wait for 5 seconds
+#   And I create a new post with text "This is a new post for scenario 1"  
+#   And I wait for 5 seconds
+#   And I publish my new post
+#   And I wait for 5 seconds
+#   And I return to dashboard
+#   And I wait for 5 seconds
+#   And I search for published posts with text "This is a new post"
+#   And I wait for 3 seconds
+#   And I modify the post writing "This post is modified for scenario 1"
+#   And I wait for 3 seconds
+#   And I publish my modified post
+#   And I wait for 3 seconds
+#   And I return to dashboard
+#   And I wait for 3 seconds
+#   Then I should see the modified post with text "This post is modified for scenario 1"
+
+
+#  @user2 @web
+#  Scenario: Como administrador me gustaria crear post, publicarlo, y luego eliminarlo
+#   Given I navigate to page "<GHOST_URL>"
+#   And I wait for 5 seconds
+#   When I make login with "<USER>" and "<PASSWORD>"
+#   And I wait for 5 seconds
+#   And I create a new post with text "This is a post for scenario 2"  
+#   And I wait for 5 seconds
+#   And I publish my new post
+#   And I wait for 5 seconds
+#   And I return to dashboard
+#   And I wait for 5 seconds
+#   And I search for published posts with text "This is a post for scenario 2"
+#   And I wait for 3 seconds
+#   And I delete the post
+#   And I wait for 5 seconds
+#   Then I shouldnt see the deleted post
+
+ @user3 @web
+ Scenario: Como administrador me gustaria crear post, dejarlo en draft, luego publicarlo y validar que se publique
   Given I navigate to page "<GHOST_URL>"
-  And I wait for 3 seconds
-  When I enter correo "<USER>"
-  And I wait for 3 seconds
-  And I enter clave "<PASSWORD>"
-  And I wait for 3 seconds
-  And I click sign in
-  And I wait for 3 seconds
-  And I select option posts
-  And I wait for 3 seconds
-  And I select New post
-  And I wait for 3 seconds
-  And I write a new post with title "This is a new post"
+  And I wait for 5 seconds
+  When I make login with "<USER>" and "<PASSWORD>"
+  And I wait for 5 seconds
+  And I create a new post with text "This is a post for scenario 3"  
+  And I wait for 5 seconds
+  And I return to dashboard
+  And I wait for 5 seconds
+  And I search for draft posts with text "This is a post for scenario 3"
   And I wait for 3 seconds
   And I publish my new post
-  And I wait for 3 seconds
+  And I wait for 5 seconds
   And I return to dashboard
-  And I wait for 3 seconds
-  And I search for published posts with text "This is a new post"
-  And I wait for 3 seconds
-  And I modify the post writing "This post is modified"
-  And I wait for 3 seconds
-  And I publish my modified post
-  And I wait for 3 seconds
-  And I return to dashboard
-  And I wait for 3 seconds
-  Then I should see the modified post with text "This post is modified"
-
+  Then I should see the published post with text "This is a post for scenario 3"
+  
--- PruebasKraken/features/web/step_definitions/posts_steps.js
+++ PruebasKraken/features/web/step_definitions/posts_steps.js
@@ -33,6 +33,32 @@ When('I write a new post with title {string}', async function(text) {
     return await element2.click();
 });
 
+When('I make login with {kraken-string} and {kraken-string}', async function(email,password){
+    let element1 = await this.driver.$('[name="identification"]');
+    await element1.setValue(email);
+    let element2 = await this.driver.$('[name="password"]');
+    await element2.waitForDisplayed({ timeout: 3000 });
+    await element2.setValue(password);
+    let element3 = await this.driver.$('span*=Sign in');
+    await element3.waitForDisplayed({ timeout: 3000 });
+    return await element3.click();
+});
+When('I create a new post with text {string}', async function(text){
+    let element1 = await this.driver.$('//li/a[@href="#/posts/"]');
+    await element1.waitForDisplayed({ timeout: 10000 });
+    await element1.click();
+    let element2 = await this.driver.$('span=New post');
+    await element2.waitForDisplayed({ timeout: 3000 });
+    await element2.click();
+    let element3 = await this.driver.$('//textarea[@placeholder="Post title"]');
+    await element3.waitForDisplayed({ timeout: 3000 });
+    await element3.setValue(text);
+    let element4 = await this.driver.$('//div[@class="gh-editor-feature-image-container"]')
+    await element4.waitForDisplayed({ timeout: 3000 });
+    return await element4.click();
+});
+
+
 When('I publish my new post', async function(){
     let element = await this.driver.$("//span[contains(text(),'Publish')]");
     await element.click();
@@ -42,8 +68,8 @@ When('I publish my new post', async function(){
 });
 
 When('I return to dashboard', async function(){
-    let element = await this.driver.$("//div/a/span[text()='Posts']");
-    return await element.click();
+    let element1 = await this.driver.$("//div/a/span[text()='Posts']");
+    return await element1.click();
 });
 
 When('I search for published posts with text {string}', async function(text){
@@ -75,3 +101,32 @@ Then('I should see the modified post with text {string}', async function(text){
     expect(element2).to.equal(true);
 });
 
+When('I delete the post', async function(){
+    let element1 = await this.driver.$("//button[@title='Settings']");
+    await element1.click();
+    let element2 = await this.driver.$("//*/*[contains(text(),'Delete')]");
+    await element2.waitForDisplayed({ timeout: 3000 });
+    await element2.click();
+    let element3 = await this.driver.$("//div/button/span[contains(text(),'Delete')]");
+    await element3.waitForDisplayed({ timeout: 3000 });
+    return await element3.click();
+});
+
+Then('I shouldnt see the deleted post', async function(){
+    let element1 = await this.driver.$("//a[@title='Dashboard']").isDisplayed();
+    expect(element1).to.equal(true);
+});
+
+When('I search for draft posts with text {string}', async function(text){
+    let element1 = await this.driver.$("//a[@title='Drafts']");
+    await element1.click();
+    let element2 = await this.driver.$(`h3*=${text}`);
+    return element2.click();
+});
+
+Then('I should see the published post with text {string}', async function(text){
+    let element1 = await this.driver.$("//a[@title='Published']");
+    await element1.click();
+    let element2 = await this.driver.$(`h3*=${text}`).isDisplayed();
+    expect(element2).to.equal(true);
+});
